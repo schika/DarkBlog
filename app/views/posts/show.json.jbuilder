7399a1a88c30cd0140a1ba49c7179bc0d21a57b0
schika <schika4@gmail.com>
2014-04-19 11:55:37 +0300

    Initialize

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :title, :text, :date, :created_at, :updated_at
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DarkBlog::Application.config.session_store :cookie_store, key: '_DarkBlog_session'
